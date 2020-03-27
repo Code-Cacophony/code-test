729704eb4b6f4de7d2447ce35a6a03f2bfd59093
Code-Cacophony <57301331+Code-Cacophony@users.noreply.github.com>
2020-03-27 13:51:01 -0700

    add more javascript functionality to program

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,15 @@ $(document).ready(function () {
         event.preventDefault();
         var age = parseInt($("#age").val());
         var gender = parseInt($("#gender").val());
-    })
-})
+        var animal = parseInt($("#favorite-animal").val());
+        var band = parseInt($("#favorite-band").val());
+        var food = parseInt($("#favorite-food").val());
+
+        if (age <= 18 || gender === 1 || animal === 4 && band === 7) {
+            $("#ruby").show();
+        } else if (age >= 18 && gender === 1 || animal === 5 && band === 8) {
+            $("#python").show();
+        }
+
+    });
+});
