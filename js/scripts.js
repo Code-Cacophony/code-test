4500f901df1603d4ff01d55207cb7b0a32b220b5
Code-Cacophony <57301331+Code-Cacophony@users.noreply.github.com>
2020-03-27 13:16:49 -0700

    begin javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 $(document).ready(function () {
-
+    $("form#user-input").submit(function (event) {
+        event.preventDefault();
+        var age = parseInt($("#age").val());
+        var gender = parseInt($("#gender").val());
+    })
 })
